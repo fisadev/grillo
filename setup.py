0df8ba9c8122d312ab50f95767428eda912f2e97
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-02-25 22:54:58 -0300

    Added setup.py to make it installable

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import os.path
+from setuptools import setup, find_packages
+
+
+# https://packaging.python.org/guides/single-sourcing-package-version/
+version = {}
+with open(os.path.join("grillo", "version.py")) as fp:
+    exec(fp.read(), version)
+
+
+setup(
+    name="Grillo",
+    version=version["__version__"],
+    author="Juan Pedro Fisanotti",
+    author_email="fisadev@gmail.com",
+    description=("A small tool to easily send data (files, clipboard) between computers with 0 "
+                 "config, just using audio and mic."),
+    long_description=open("README.md").read(),
+    packages=find_packages(exclude=["tests"]),
+    url="https://github.com/fisadev/grillo",
+    entry_points = {
+        'console_scripts': ['grillo=grillo.grillo:main'],
+    },
+    install_requires=[
+        "chirpsdk",
+        "pyperclip",
+        "fire",
+        "termcolor",
+    ],
+)

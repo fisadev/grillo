c9034ccb88645a79ca8219966f3f9c9eb35be1f5
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-03-03 20:38:53 -0300

    Added setup.cfg, and extra fields to setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,11 @@ setup(
     version=version["__version__"],
     author="Juan Pedro Fisanotti",
     author_email="fisadev@gmail.com",
+    license="GPLv3",
     description=("A small tool to easily send data (files, clipboard) between computers with 0 "
                  "config, just using audio and mic."),
     long_description=open("README.md").read(),
+    keywords=["audio", "modem", "clipboard", "transfer"],
     packages=find_packages(exclude=["tests"]),
     url="https://github.com/fisadev/grillo",
     entry_points = {

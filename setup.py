e20618198898f028dffc56b3a4b98a4431b4c56f
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-03-04 15:12:17 -0300

    Up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="GrilloModem",
-    version='1.1.0',
+    version='1.1.1',
     author="Juan Pedro Fisanotti",
     author_email="fisadev@gmail.com",
     license="GPLv3",
